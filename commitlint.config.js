a2f8098627a0544752f98d8470cf208b20aae66d
Ronaldo Chan <rpchan44@gmail.com>
2026-07-24 09:02:28 +0800

    feat: protection (#13)
    * feat: protection
    
    * feat: test
    
    * Refactor commitlint workflow for better validation
    
    Updated commitlint workflow to use new actions and improve commit message validation.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   rules: {
+     'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test', 'perf'],
+    ],
     'header-pattern': [2, 'always', /^(feat|fix|chore|docs|style|refactor|test|perf): .+/],
     'header-max-length': [2, 'always', 72],
   },
